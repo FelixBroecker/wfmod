8cec3030a499f470d339a55d1e346c3616974f86
FelixBroecker <felixbroecker90@gmail.com>
2025-07-04 11:29:52 +0200

    Remove shifted functions

--- main.py
+++ main.py
@@ -260,123 +260,19 @@ def main():
         ...
 
     elif data["WavefunctionOptions"]["wavefunctionOperation"] == "add_singles":
-        aS = AddSingles()
-        aS.add_singles(
-            N,
-            S,
-            M_s,
-            n_MO,
-            initial_determinant,
-            orbital_symmetry,
-            point_group,
-            frozen_electrons,
-            frozen_MOs,
-            wavefunction_name,
-            wftype,
-        )
+        ...
 
     elif data["WavefunctionOptions"]["wavefunctionOperation"] == "read_cipsi":
-        wf_name_praefix = wavefunction_name.split(".")[0]
-        # parse determinants and print them in AMOLQC format
-        ci_coefficients, determinants = utils.parse_cipsi_dets(
-            wavefunction_name
-        )
-        sCI.write_AMOLQC(
-            [],
-            determinants[:split_at],
-            ci_coefficients[:split_at],
-            pretext="",
-            file_name=f"{wf_name_praefix}_dets.wf",
-            wftype="det",
-        )
-        print(len(determinants))
-
-        # get csfs from determinant basis and print wavefunction.
-        # create guess for CI coefficients
-        csf_coefficients, csfs = sCI.get_unique_csfs(determinants, S, M_s)
-        csf_coefficients, csfs = sCI.sort_determinants_in_csfs(
-            csf_coefficients, csfs
-        )
-        ci_csf_coefficients = [1 if n == 0 else 0 for n in range(len(csfs))]
-
-        sCI.write_AMOLQC(
-            csf_coefficients[:split_at],
-            csfs[:split_at],
-            ci_csf_coefficients[:split_at],
-            pretext="",
-            file_name=f"{wf_name_praefix}_csfs.wf",
-        )
-        print(f"len csfs: {len(csfs)}")
-
-        # expand again in determinants to see how may
-        # determinants have been added
-        _, _, determinant_basis_csfs = sCI.get_transformation_matrix(
-            csf_coefficients, csfs, range(len(csf_coefficients))
-        )
-        print(len(determinant_basis_csfs))
+        ...
 
     elif data["WavefunctionOptions"]["wavefunctionOperation"] == "eval":
         """Evaluate blockwise optimization."""
-
-        try:
-            print("Create directory eval.")
-            mkdir("eval")
-        except FileExistsError:
-            FileExistsError("Directory eval already exists.")
-
-        # evaluate energy course during blockwise optimization
-        energy_course_data = evaluation.get_energy_course()
-        with open("eval/energy_course.txt", "w") as reffile:
-            for line in energy_course_data:
-                reffile.write(f"{line}\n")
-
-        # evaluate excitaions degree of CSFs per wavefunction 1f each block
-
-        list_of_excitation_lists = []
-        list_of_counter_lists = []
-        list_of_wavefunction_names = []
-
-        for file in files():
-            if file.endswith(".wf"):
-                # get wavefunction name
-                wavefunction_name = file.split(".")[0]
-
-                try:
-                    degree_of_excitation, counter = (
-                        evaluation.get_excitations_degree(
-                            N,
-                            wavefunction_name,
-                            initial_determinant,
-                            wftype,
-                            max_degree=20,
-                            print_file=True,
-                            verbose=False,
-                        )
-                    )
-                    list_of_excitation_lists.append(degree_of_excitation)
-                    list_of_counter_lists.append(counter)
-                    list_of_wavefunction_names.append(wavefunction_name)
-                except Exception as e:
-                    continue
-
-        with open("eval/excitation.json", "w") as reffile:
-            json.dump(
-                {
-                    "wavefunction_name": list_of_wavefunction_names,
-                    "degree_of_excitation": list_of_excitation_lists,
-                    "counter": list_of_counter_lists,
-                },
-                reffile,
-            )
+        ...
 
     if data["Output"]["plotCICoefficients"]:
-        if data["Output"]["plotly"]:
-            evaluation.plot_ci_coefficients_plotly(wavefunction_name, N, n_MO)
-        else:
-            evaluation.plot_ci_coefficients(wavefunction_name, N)
+        ...
 
 
 # program starts
-
 
 main()
