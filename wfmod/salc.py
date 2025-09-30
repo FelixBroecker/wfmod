b63b16d317acc7cfe71427f13054f40ff1bbf70d
FelixBroecker <felixbroecker90@gmail.com>
2025-09-30 11:10:44 +0200

    Add white spaces

--- wfmod/salc.py
+++ wfmod/salc.py
@@ -283,7 +283,7 @@ class SALC:
             "1 sd'": ["py1 -> +px1", "py2 -> +px2"],
             "1 sd''": ["py1 -> -px1", "py2 -> -px2"],
         }
-        py_reducable_basis = [2,0,0,-2,0,0,0,0,0,0,0,0,2,2,-2,-2,]
+        py_reducable_basis = [2, 0, 0, -2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, -2, -2]
         # convert to transformation matrix
         for mulliken, operations in py_orbs.items():
             py_orbs[mulliken] = self.get_transformation_matrix(
@@ -334,7 +334,7 @@ class SALC:
             "1 sd''": ["pi_x -> -pi_y"],
         }
         orb_empty = np.zeros((len(pi_orbital_basis), len(pi_orbital_basis)))
-        pi_x_u_reducable_basis = [1,0,0,-1,+1,-1,0,0,-1,0,0,1,+1,-1,0,0,]
+        pi_x_u_reducable_basis = [1, 0, 0, -1, +1, -1, 0, 0, -1, 0, 0, 1, +1, -1, 0, 0]
         # convert to transformation matrix
         for mulliken, operations in pi_x__u_orbs.items():
             pi_x__u_orbs[mulliken] = self.get_transformation_matrix(
@@ -359,7 +359,7 @@ class SALC:
             "1 sd'": ["pi_y -> +pi_x"],
             "1 sd''": ["pi_y -> -pi_x"],
         }
-        pi_y_u_reducable_basis = [1,0,0,-1,-1,1,0,0,-1,0,0,1,-1,1,0,0,]
+        pi_y_u_reducable_basis = [1, 0, 0, -1, -1, 1, 0, 0, -1, 0, 0, 1, -1, 1, 0, 0]
         # convert to transformation matrix
         for mulliken, operations in pi_y__u_orbs.items():
             pi_y__u_orbs[mulliken] = self.get_transformation_matrix(
@@ -384,7 +384,7 @@ class SALC:
             "1 sd'": ["pi_x -> +pi_y"],
             "1 sd''": ["pi_x -> -pi_y"],
         }
-        pi_x_g_reducable_basis = [1,0,0,-1,1,-1,0,0,+1,0,0,-1,+1,-1,0,0,]
+        pi_x_g_reducable_basis = [1, 0, 0, -1, 1, -1, 0, 0, +1, 0, 0, -1, +1, -1, 0, 0]
         # convert to transformation matrix
         for mulliken, operations in pi_x_g_orbs.items():
             pi_x_g_orbs[mulliken] = self.get_transformation_matrix(
@@ -409,7 +409,7 @@ class SALC:
             "1 sd'": ["pi_y -> +pi_x"],
             "1 sd''": ["pi_y -> -pi_x"],
         }
-        pi_y_g_reducable_basis = [1,0,0,-1,-1,1,0,0,+1,0,0,-1,-1,1,0,0,]
+        pi_y_g_reducable_basis = [1, 0, 0, -1, -1, 1, 0, 0, +1, 0, 0, -1, -1, 1, 0, 0]
         # convert to transformation matrix
         for mulliken, operations in pi_y_g_orbs.items():
             pi_y_g_orbs[mulliken] = self.get_transformation_matrix(
@@ -435,7 +435,7 @@ class SALC:
             "1 sd''": ["dxy1 -> +dxy1", "dxy2 -> +dxy2"],
         }
         orb_empty = np.zeros((len(d_orbital_basis), len(d_orbital_basis)))
-        dxy_reducable_basis = [2,-2,-2,2,0,0,0,0,0,0,0,0,-2,-2,+2,+2,]
+        dxy_reducable_basis = [2, -2, -2, 2, 0, 0, 0, 0, 0, 0, 0, 0, -2, -2, +2, +2]
 
         dxz_orbs = {
             "1 E": ["dxz1 -> +dxz1", "dxz2 -> +dxz2"],
@@ -455,7 +455,7 @@ class SALC:
             "1 sd'": ["dxz1 -> +dyz1", "dxz2 -> +dyz2"],
             "1 sd''": ["dxz1 -> -dyz1", "dxz2 -> -dxz2"],
         }
-        dxz_reducable_basis = [2,0,0,-2,0,0,0,0,0,0,0,0,2,-2,+2,-2,]
+        dxz_reducable_basis = [2, 0, 0, -2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, +2, -2]
 
         dyz_orbs = {
             "1 E": ["dyz1 -> +dyz1", "dyz2 -> +dyz2"],
@@ -475,7 +475,7 @@ class SALC:
             "1 sd'": ["dyz1 -> +dxz1", "dyz2 -> +dxz2"],
             "1 sd''": ["dyz1 -> -dxz1", "dyz2 -> -dxz2"],
         }
-        dyz_reducable_basis = [2,0,0,-2,0,0,0,0,0,0,0,0,-2,-2,2,2,]
+        dyz_reducable_basis = [2, 0, 0, -2, 0, 0, 0, 0, 0, 0, 0, 0, -2, -2, 2, 2]
 
         dxxyy_orbs = {
             "1 E": ["dxxyy1 -> +dxxyy1", "dxxyy2 -> +dxxyy2"],
@@ -495,7 +495,7 @@ class SALC:
             "1 sd'": ["dxxyy1 -> -dxxyy1", "dxxyy2 -> -dxxyy2"],
             "1 sd''": ["dxxyy1 -> -dxxyy1", "dxxyy2 -> -dxxyy2"],
         }
-        dxxyy_reducable_basis = [2,-2,-2,2,0,0,0,0,0,0,0,0,2,2,-2,-2,]
+        dxxyy_reducable_basis = [2, -2, -2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, -2, -2]
 
         dzz_orbs = {
             "1 E": ["dzz1 -> +dzz1", "dzz2 -> +dzz2"],
@@ -535,7 +535,7 @@ class SALC:
             "1 sd'": ["dxx1 -> +dyy1", "dxx2 -> +dyy2"],
             "1 sd''": ["dxx1 -> +dyy1", "dxx2 -> +dyy2"],
         }
-        dxx_reducable_basis = [2,0,0,2,0,0,0,0,0,0,0,0,2,2,0,0,]
+        dxx_reducable_basis = [2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0,]
 
         dyy_orbs = {
             "1 E": ["dyy1 -> +dyy1", "dyy2 -> +dyy2"],
@@ -555,7 +555,7 @@ class SALC:
             "1 sd'": ["dyy1 -> +dxx1", "dyy2 -> +dxx2"],
             "1 sd''": ["dyy1 -> +dxx1", "dyy2 -> +dxx2"],
         }
-        dyy_reducable_basis = [2,0,0,2,0,0,0,0,0,0,0,0,2,2,0,0,]
+        dyy_reducable_basis = [2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0,]
 
         dzz_cart_orbs = {
             "1 E": ["dzz1 -> +dzz1", "dzz2 -> +dzz2"],
@@ -575,7 +575,7 @@ class SALC:
             "1 sd'": ["dzz1 -> +dzz1", "dzz2 -> +dzz2"],
             "1 sd''": ["dzz1 -> +dzz1", "dzz2 -> +dzz2"],
         }
-        dzz_cart_reducable_basis = [2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2,]
+        dzz_cart_reducable_basis = [2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2,]
         cart_bas = [
             dyz_orbs,
             dxz_orbs,
