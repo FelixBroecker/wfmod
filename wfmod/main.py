018fad1db93d4cd282a5b525e652a68dea5219ac
FelixBroecker <felixbroecker90@gmail.com>
2025-10-14 17:49:08 +0200

    Add parsing of abreviated ranges in lists in input file

--- wfmod/main.py
+++ wfmod/main.py
@@ -21,6 +21,48 @@ def header():
     print(" " + "=" * 40)
     print()
 
+def range_to_list(contracted_list: list) -> list:
+    """Expand ranges in a list to full lists. E.g. ['1-2', 3, 5] -> [1, -1, 2, -2, 3, 5]"""
+    for item in contracted_list:
+        if isinstance(item, str) and "-" in item:
+            start, end = map(int, item.split("-"))
+            expanded = list(range(start, end + 1)) + list(range(-start, -end - 1, -1))
+            contracted_list.remove(item)
+            contracted_list.extend(expanded)
+    contracted_list = sorted(contracted_list, key=abs)
+    return contracted_list
+
+def parse_input(input_data):
+    """Parse input file and convert data if necessary."""
+    for i, func in enumerate(input_data["pipeline"]):
+
+        # check if short notation was used in frozen_MOs
+        frozen = func["args"].get("frozen_MOs")
+        if frozen and any(isinstance(x, str) for x in frozen):
+            input_data["pipeline"][i]["args"]["frozen_MOs"] = range_to_list(frozen)
+
+        # check if short notation was used in frozen_electrons
+        frozen = func["args"].get("frozen_electrons")
+        if frozen and any(isinstance(x, str) for x in frozen):
+            input_data["pipeline"][i]['args']["frozen_electrons"] = range_to_list(frozen)
+
+        # check if short notation was used in initial_determinant
+        det = func["args"].get("initial_determinant")
+        if det and any(isinstance(x, str) for x in det):
+            input_data["pipeline"][i]['args']["initial_determinant"] = range_to_list(det)
+        # check if short notation was used in excitations
+        # must be performed after expanding frozen_MOs and frozen_electrons
+        excitations = func["args"].get("excitations")
+        if excitations and "fci" in excitations:
+            n_frozen_elec = len(func["args"]["frozen_electrons"])
+            n_frozen_orb = len(func["args"]["frozen_MOs"])
+            n_elec = func["args"]["n_electrons"] - n_frozen_elec
+            n_orb = func["args"]["n_orbitals"] - n_frozen_orb // 2
+            max_exc = min(n_elec,  2 * n_orb - n_elec)
+            input_data["pipeline"][i]['args']["excitations"] = [n for n in range(1, max_exc + 1)]
+
+    return input_data
+
 
 def main():
     FUNCTIONS = {
@@ -59,6 +101,7 @@ def main():
     # read input file
     with open(input_file, "r") as reffile:
         input_data = yaml.safe_load(reffile)
+    input_data = parse_input(input_data)
 
     # call functions from input data
     cls = None
