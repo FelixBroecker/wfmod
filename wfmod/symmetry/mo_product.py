7f734c25ed67406c9d263ebb8d3c269998314085
FelixBroecker <felixbroecker90@gmail.com>
2025-10-14 18:41:24 +0200

    Add test for MO product class

--- wfmod/symmetry/mo_product.py
+++ wfmod/symmetry/mo_product.py
@@ -1,6 +1,9 @@
-from salc import SALC
-
-
 class MOProduct():
     def __init__(self,):
         ...
+
+    def print_mos(self, mo1, mo2):
+        print("MO 1:")
+        print(mo1)
+        print("MO 2:")
+        print(mo2)
