15275569d82e53f6f576d8e45695bd6e1afb2143
FelixBroecker <felixbroecker90@gmail.com>
2025-10-18 15:33:49 +0200

    Restructure

--- tests/test_mo_product.py
+++ tests/test_mo_product.py
@@ -7,7 +7,13 @@ from wfmod.symmetry.mo_product import MOProduct
 def moProd():
     """Fixture to provide a MOProduct instance for all tests"""
     point_group = "d2h"
-    mo_basis = ["C1_1s", "C1_2s", "C1_3s", "C1_4s", "C1_5s", "C1_1px", "C1_1py", "C1_1pz", "C1_2px", "C1_2py", "C1_2pz", "C1_3px", "C1_3py", "C1_3pz", "C1_1dzz", "C1_1dxz", "C1_1dyz", "C1_1dxxyy", "C1_1dxy", "C2_1s", "C2_2s", "C2_3s", "C2_4s", "C2_5s", "C2_1px", "C2_1py", "C2_1pz", "C2_2px", "C2_2py", "C2_2pz", "C2_3px", "C2_3py", "C2_3pz", "C2_1dzz", "C2_1dxz", "C2_1dyz", "C2_1dxxyy", "C2_1dxy"]
+    mo_basis = [
+        "C1_1s", "C1_2s", "C1_3s", "C1_4s", "C1_5s", "C1_1px", "C1_1py", "C1_1pz",
+        "C1_2px", "C1_2py", "C1_2pz", "C1_3px", "C1_3py", "C1_3pz", "C1_1dzz", "C1_1dxz",
+        "C1_1dyz", "C1_1dxxyy", "C1_1dxy", "C2_1s", "C2_2s", "C2_3s", "C2_4s", "C2_5s", "C2_1px",
+        "C2_1py", "C2_1pz", "C2_2px", "C2_2py", "C2_2pz", "C2_3px", "C2_3py", "C2_3pz", "C2_1dzz",
+        "C2_1dxz", "C2_1dyz", "C2_1dxxyy", "C2_1dxy"
+        ]
     moProd = MOProduct(point_group, mo_basis)
     return moProd
 
